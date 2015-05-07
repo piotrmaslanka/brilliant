490c8014be972fd87f59072e3a2af8c35fbc628f
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2015-05-07 12:57:19 +0200

    Autowire changed to wire, added Session support

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,26 +1,26 @@
 """
 Component decorator class
 """
-import threading
+import threading, uuid
 
 class GlobalDatabase(object):
     """Brilliant's database of stuff"""
     
     CurrentScope = None
     Singletons = {}     # name => Singleton instance
-    ThreadLocal = threading.local()        # name => Scope instance    
-    ThreadLocal.Scopes = []  ## list of scopes, from oldest (0) to newest (last)
+    Sessions = {}       # name => Scope instance - session scopes
+    ThreadLocal = threading.local()
+    ThreadLocal.RequestScope = None     # current request scope
+    ThreadLocal.SessionScope = None     # current session scope
 
-
-def Autowire(name):
-    try:
+def wire(name):
+    try:    # Look in GLOBAL scope
         return GlobalDatabase.Singletons[name]
     except KeyError:
-        for scope in reversed(GlobalDatabase.ThreadLocal.Scopes):
-            try:
-                return scope.Objects[name]
-            except KeyError:
-                pass
+        try:        # Look in SESSION scope
+            return GlobalDatabase.ThreadLocal.SessionScope.Objects[name]
+        except KeyError:
+            pass
             
     raise NameError
 
@@ -28,6 +28,7 @@ class Component(object):
     """Use as a decorator to denote Brilliant components"""
     
     SCOPE_SINGLETON = 0
+    SCOPE_SESSION = 1
         
     def __init__(self, name, **kwargs):
         self.name = name
@@ -42,23 +43,53 @@ class Component(object):
             
             if self.scope == Component.SCOPE_SINGLETON:
                 GlobalDatabase.Singletons[self.name] = inst
+            elif self.scope == Component.SCOPE_SESSION:
+                curSess = GlobalDatabase.ThreadLocal.SessionScope
+                curSess.Objects[self.name] = inst
                                 
             return inst
         
         return instantiator
+    
+class Session(object):
+    """A session scope. This is a scope that will be created on demand, and will be
+    deleted only when user wishes it. Use it like..
+    
+    session = Session.create()
+    print 'Key is %s' % (session.key, )
+    
+    with session:
+        ... use objects with session scope ...
         
+    session.destroy()    
+    """
+    
+    @staticmethod
+    def create(self):
+        key = uuid.uuid4().hex
+        ss = Session(key)
+        GlobalDatabase.Sessions[key] = ss
+        return ss        
         
-class Scope(object):
-    """Just a scope"""
-    def __init__(self, name):
-        self.name = name        
-        self.Objects = {}       # name => instance
-
+    def __init__(self, key):
+        self.key = key
+        self.Objects = {}
+        
+    @staticmethod
+    def get(self, key):
+        """Return a session scope by it's key
+        @raise KeyError if does not exist"""
+        return GlobalDatabase.Sessions[key]        
+        
     def __enter__(self):
-        """Enter a new scope"""
-        GlobalDatabase.ThreadLocal.Scopes.append(self)
+        """Pick this session as current context"""
+        GlobalDatabase.ThreadLocal.SessionScope = self
         
-    def __exit__(self, a, b, c):
-        del GlobalDatabase.ThreadLocal.Scopes[-1]
-        return False
+    def __exit__(self):
+        GlobalDatabase.ThreadLocal.SessionScope = None
+        
+    def destroy(self):
+        """Destroy this session"""
+        del GlobalDatabase.Sessions[self.key]
+        
         
